4556aa5c91e26ebe9dbe1b1ffb5689e49afbd1b4
Mohamad Chaaban <chaa0026@algonquinlive.com>
2022-02-09 16:32:34 -0500

    Changes: - Controller now implements ActionListener and only has one actionPerformed method - added a reset method that will reset the game when called on - added a time method that will increment the time and update the UI when called on TODO: Make the view like the prof said instead of having view actions coded in the controller

--- Controller.java
+++ Controller.java
@@ -5,101 +5,90 @@ import javax.swing.Timer;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Controller {
-    private ActionListener timeTask;
+public class Controller implements ActionListener {
     private int score = 0;
     private int seconds = 0;
     private int minutes = 0;
-    
+    private Frame mainFrame;
     private JFrame console = new JFrame("Console");
     private JFrame chat = new JFrame("Chat");
 
     Controller(Frame frame) {
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
-                frame.getGridPanel().getGridButtons()[i][j].addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        JButton button = (JButton) e.getSource();
-                        int row = 0;
-                        int col = 0;
-                        // to be changed to actual scoring in the future
-                        score++;
-                        frame.getFooterPanel().getScoreLabel().setText("Score: " + score);
-                        for (int i = 0; i < 5; i++) {
-                            for (int j = 0; j < 5; j++) {
-                                if (frame.getGridPanel().getGridButtons()[i][j] == button) {
-                                    row = i + 1;
-                                    col = j + 1;
-                                }
-                            }
-                        }
-                        System.out.println("row: " + row + " col: " + col);
-                    }
-                });
-            }
-        }
-        frame.getFooterPanel().getResetButton().addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                reset();
-            }
+        mainFrame = frame;
+        for (int i = 0; i < 5; i++)
+            for (int j = 0; j < 5; j++)
+                frame.getGridPanel().getGridButtons()[i][j].addActionListener(this);
+        frame.getFooterPanel().getResetButton().addActionListener(this);
+        frame.getFooterPanel().getConsoleButton().addActionListener(this);
+        frame.getFooterPanel().getChatButton().addActionListener(this);
+        frame.getFooterPanel().getEraseButton().addActionListener(this);
+        frame.getFooterPanel().getMarkButton().addActionListener(this);
+        frame.getFooterPanel().getCheckButton().addActionListener(this);
+        new Timer(1000, this).start();
+    }
 
-            private void reset() {
-                score = 0;
-                seconds = 0;
-                minutes = 0;
-                frame.getFooterPanel().getTimerLabel().setText("Timer: 00:00");
-                frame.getFooterPanel().getScoreLabel().setText("Score: 0");
-                frame.getTopPanel().reset();
-                frame.getSidePanel().reset();
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getActionCommand() == null)
+            time();
+        else
+            switch (e.getActionCommand()) {
+                case "Reset":
+                    reset();
+                    break;
+                case "Console":
+                    console.setSize(306, 920);
+                    console.setLocation(mainFrame.getX() - console.getWidth() - 10, mainFrame.getY());
+                    console.setVisible(console.isVisible() ? false : true);
+                    break;
+                case "Chat":
+                    chat.setSize(306, 920);
+                    chat.setLocation(mainFrame.getX() + mainFrame.getWidth() + 10, mainFrame.getY());
+                    chat.setVisible(chat.isVisible() ? false : true);
+                    break;
+                case "Erase":
+                    System.out.println("Erase button clicked");
+                    break;
+                case "Mark":
+                    System.out.println("Mark button clicked");
+                    break;
+                case "Check":
+                    System.out.println("Check button clicked");
+                    break;
+                default:
+                    JButton button = (JButton) e.getSource();
+                    int row = 0;
+                    int col = 0;
+                    // to be changed to actual scoring in the future
+                    score++;
+                    mainFrame.getFooterPanel().getScoreLabel().setText("Score: " + score);
+                    for (int i = 0; i < 5; i++)
+                        for (int j = 0; j < 5; j++)
+                            if (mainFrame.getGridPanel().getGridButtons()[i][j] == button) {
+                                row = i + 1;
+                                col = j + 1;
+                            }
+                    System.out.println("row: " + row + " col: " + col);
+                    break;
             }
-        });
-        frame.getFooterPanel().getConsoleButton().addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                console.setSize(306, 920);
-                console.setLocation(frame.getX() - console.getWidth() - 10, frame.getY());
-                console.setVisible(console.isVisible() ? false : true);
-            }
-        });
-        frame.getFooterPanel().getChatButton().addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                chat.setSize(306, 920);
-                chat.setLocation(frame.getX() + frame.getWidth() + 10, frame.getY());
-                chat.setVisible(chat.isVisible() ? false : true);
-            }
-        });
-        frame.getFooterPanel().getEraseButton().addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("Erase button clicked");
-            }
-        });
-        frame.getFooterPanel().getMarkButton().addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("Mark button clicked");
-            }
-        });
-        frame.getFooterPanel().getCheckButton().addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("Check button clicked");
-            }
-        });
-        timeTask = new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                seconds++;
-                if (seconds == 60) {
-                    seconds = 0;
-                    minutes++;
-                }
-                frame.getFooterPanel().getTimerLabel().setText(String.format("Timer: %02d:%02d", minutes, seconds));
-            }
-        };
-        new Timer(1000, timeTask).start();
+    }
+
+    private void reset() {
+        score = 0;
+        seconds = 0;
+        minutes = 0;
+        mainFrame.getFooterPanel().getTimerLabel().setText("Timer: 00:00");
+        mainFrame.getFooterPanel().getScoreLabel().setText("Score: 0");
+        mainFrame.getTopPanel().reset();
+        mainFrame.getSidePanel().reset();
+    }
+
+    private void time() {
+        seconds++;
+        if (seconds == 60) {
+            seconds = 0;
+            minutes++;
+        }
+        mainFrame.getFooterPanel().getTimerLabel().setText(String.format("Timer: %02d:%02d", minutes, seconds));
     }
 }
