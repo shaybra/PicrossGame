207796bd59b548ce5fa79a1274ab2c02c6b3bef3
Mohamad Chaaban <chaa0026@algonquinlive.com>
2022-02-09 14:42:37 -0500

    Changes: - Added a random number for the side and top hints instead of what we had before. - changed the empty berder in the logo panel to 27 for top, right, buttom, and left so it looks better

--- SidePanel.java
+++ SidePanel.java
@@ -5,14 +5,19 @@ import javax.swing.SwingConstants;
 import java.awt.GridLayout;
 import java.awt.Color;
 
+import java.util.Random;
+import java.lang.StringBuilder;
+
 public class SidePanel extends JPanel{
     JLabel hints[][] = new JLabel[5][3];
+    Random rand = new Random();
     
     SidePanel(){
         setLayout(new GridLayout(5,3,5,0));
         for(int i=0;i<5;i++){
             for(int j=0;j<3;j++){
-                hints[i][j] = new JLabel("Hint"+"("+(i+1)+","+(j+1)+")");
+                int number = rand.nextInt(6);
+                hints[i][j] = number != 0 ? new JLabel(new StringBuilder().append(number).toString()) : new JLabel();
                 hints[i][j].setForeground(new Color(0xFFFFFF));
                 hints[i][j].setHorizontalAlignment(SwingConstants.CENTER);
                 hints[i][j].setVerticalAlignment(SwingConstants.CENTER);
--- TopPanel.java
+++ TopPanel.java
@@ -7,22 +7,27 @@ import java.awt.GridLayout;
 import java.awt.Color;
 import java.awt.BorderLayout;
 
+import java.util.Random;
+import java.lang.StringBuilder;
+
 public class TopPanel extends JPanel {
     JLabel hints[][] = new JLabel[3][5];
+    Random rand = new Random();
 
     TopPanel() {
         setLayout(new BorderLayout());
         JPanel logoPanel = new JPanel();
         JLabel logo = new JLabel("<html><h1>M&G</h1></html>");
         logo.setForeground(new Color(0x2364C7));
-        logoPanel.setBorder(BorderFactory.createEmptyBorder(30, 30, 30, 30));
+        logoPanel.setBorder(BorderFactory.createEmptyBorder(27, 27, 27, 27));
         logoPanel.add(logo);
         add(logoPanel, BorderLayout.WEST);
         JPanel hintsPanel = new JPanel();
         hintsPanel.setLayout(new GridLayout(3, 5));
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 5; j++) {
-                hints[i][j] = new JLabel("Hint" + "(" + (i + 1) + "," + (j + 1) + ")");
+                int number = rand.nextInt(6);
+                hints[i][j] = number != 0 ? new JLabel(new StringBuilder().append(number).toString()) : new JLabel();
                 hints[i][j].setForeground(new Color(0xFFFFFF));
                 hints[i][j].setHorizontalAlignment(SwingConstants.CENTER);
                 hints[i][j].setVerticalAlignment(SwingConstants.CENTER);
